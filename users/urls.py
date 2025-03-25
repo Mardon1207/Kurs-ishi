65e709b7f6f53dd5421d95060ab8eec588144e4f
Mardon1207 <mardonsultonov8@gmail.com>
2025-03-25 23:45:17 +0500

    profile

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, CustomTokenObtainPairView
+from .views import RegisterView, CustomTokenObtainPairView,user_profile
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # ✅ To‘g‘ri login URL
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', user_profile, name='user-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import CustomUser
 from .serializers import UserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
@@ -29,3 +33,19 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_profile(request):
+    user = request.user
+    data = {
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email,
+        "username": user.username,
+        "phone": user.profile.phone if hasattr(user, 'profile') else None,
+        "region": user.profile.region if hasattr(user, 'profile') else None,
+        "district": user.profile.district if hasattr(user, 'profile') else None,
+        "profile_image": request.build_absolute_uri(user.profile.profile_image.url) if hasattr(user, 'profile') and user.profile.profile_image else None
+    }
+    return Response(data)
