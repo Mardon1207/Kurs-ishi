33816ab29246082f6e24079b881be609e64171fa
Mardon1207 <mardonsultonov8@gmail.com>
2025-03-26 22:00:34 +0500

    urls

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # ✅ To‘g‘ri login URL
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('profile/', user_profile, name='user-profile'),
+    path('profile/', user_profile, name='user_profile'),
 ]
